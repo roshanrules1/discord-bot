90c3f901f88ac91f825a015192f9994c53266fb1
parzi-val <krbala1511@gmail.com>
2021-07-08 15:22:38 +0530

    updated

--- command.py
+++ command.py
@@ -3,6 +3,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from tts import Text_to_speech
+from youtube_dl import ytdl
 
 load_dotenv()
 
@@ -10,9 +11,11 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix="$")
 
+
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}"")
+    print(f"We have logged in as {client.user}")
+
 
 @bot.command(name="say")
 async def say(ctx, *args):
@@ -24,6 +27,20 @@ async def talk(ctx, *args):
     Text_to_speech(" ".join(args))
     await ctx.send(file=discord.File("tts.mp3"))
     os.remove("tts.mp3")
+
+
+@bot.command(name="download")
+async def download(ctx, url):
+    case = ytdl(url)
+    print(case)
+    if case[0] == 1 or case[0] == 2 or case[0] == 3:
+        await ctx.send(file=discord.File("some.mp4"))
+    else:
+        await ctx.send(
+            """File too big
+        converted to mp3"""
+        )
+        await ctx.send(file=discord.File("some.mp3"))
 
 
 bot.run(os.getenv("TOKEN"))
--- youtube_dl.py
+++ youtube_dl.py
@@ -7,19 +7,20 @@ def ytdl(url):
     )
     if "some.mp4" in os.listdir():
         print("already mp4.")
+        return [1, "already mp4"]
     elif "some.mkv" in os.listdir():
         os.system(f"ffmpeg -i some.mkv -codec copy some.mp4")
         os.remove("some.mkv")
         print("converted to mp4.")
+        return [2, "converted from mkv to mp4"]
     else:
         os.system(f"ffmpeg -i some.webm -codec copy some.mp4")
         os.remove("some.webm")
         print("converted to mp4.")
+        return [3, "converted from webm to mp4"]
     if (x := os.path.getsize("some.mp4") / (1024 * 1024)) > 8:
         print(f"{x}, file size to big...")
         os.remove("some.mp4")
         os.system(f"youtube-dl -x --audio-format mp3 -o some.mp3 {url}")
         print("converted to mp3.")
-
-
-ytdl(input("url: "))
+        return [4, "converted from mp4 to mp3"]
