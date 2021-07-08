7ffe33a62d576cb3c08ab939965396884736fe30
parzi-val <krbala1511@gmail.com>
2021-07-08 14:23:57 +0530

    removed auth token

--- presence.py
+++ presence.py
@@ -1,17 +1,17 @@
-import pypresence
+from pypresence import Presence
 import time
+import os
 
-client_id = os.getenv("CLIENT_ID")
-Client = pypresence.Client(client_id, pipe=0)
-Client.start()
+client_id = os.getenv("CLIENT_ID")  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect()  # Start the handshake loop
 
 print(
-    Client.set_activity(
-        state="kinda working",
-        details="just gotta figure this cloud part",
-        large_image="gojo",
-        small_image="NAME OF SMALL IMAGE HERE",
-        large_text="fox-y",
-        start=time.time(),
+    RPC.update(
+        state="Lookie Lookie",
+        details="A test of qwertyquerty's Python Discord RPC wrapper, pypresence!",
     )
-)
+)  # Set the presence
+
+while True:  # The presence will stay on as long as the program is running
+    time.sleep(15)  # Can only update rich presence every 15 seconds
--- upload.py
+++ upload.py
@@ -5,9 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-headers = {
-    "Authorization": "Bearer ya29.a0ARrdaM93nmqj7eRns_ZzeKkjlHtZdkgEeYARKYtyYdZwTmtvZJqR-PcymbLXJIqkdZaK_pwVx_8_3_NUATWVHkaS1Z_K6cC8awCpIkInayj718hnStM1iU_s9hr330CusCcb01s0mKj93pN1E8K0tCHWi0Cl"
-}
+headers = {"Authorization": "Bearer auth token"}
 para = {
     "name": "some.mp4",
 }
