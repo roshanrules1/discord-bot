a22e5b8442a7c108ebf581644941e98f9bcd0576
parzi-val <krbala1511@gmail.com>
2021-07-08 14:21:25 +0530

    working commands

---
+++ command.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+import discord
+import os
+from dotenv import load_dotenv
+from tts import Text_to_speech
+
+load_dotenv()
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix="$")
+
+
+@bot.command(name="say")
+async def say(ctx, *args):
+    await ctx.send(f"{' '.join(args)}")
+
+
+@bot.command(name="talk")
+async def talk(ctx, *args):
+    Text_to_speech(" ".join(args))
+    await ctx.send(file=discord.File("tts.mp3"))
+    os.remove("tts.mp3")
+
+
+bot.run(os.getenv("TOKEN"))
