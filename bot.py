27984e699df85db417cdff9b04cdd0aa05e334c9
parzi-val <86968472+parzi-val@users.noreply.github.com>
2021-09-19 13:03:23 +0530

    redefined media content

--- bot.py
+++ bot.py
@@ -5,25 +5,24 @@ from dotenv import load_dotenv
 from tts import Text_to_speech
 from downloader import ytdl
 from random import choice
-from subprocess import Popen
 from youtube import youtube
-from google_ import google
-from image import get_google_img
+import json
+import aiohttp
+import random
 
-# from math_calc import calc_string
-
-photo = [
-    "chump.png",
-    "clap.gif",
-    "creep.png",
-    "headshake.gif",
-    "heheboi.png",
-    "ohno.png",
-    "ramu.png",
-    "whaat.png",
-    "woah.png",
-    "ooo.jpeg",
-]
+print(
+    """  
+   /\   /\   
+  //\\\_//\\\     ____
+  \_     _/    /   /
+   / * * \    /^^^]
+   \_\O/_/    [   ]
+    /   \_    [   /
+    \     \_  /  /
+     [ [ /  \/ _/
+    _[ [ \  /_/)"""
+)
+print(".\n.\n.\nStarting up fox-y...🦊")
 
 load_dotenv()
 
@@ -44,14 +43,14 @@ async def help(ctx):
     embed = discord.Embed(
         title="Fox-y Pluggin Commands",
         url="https://youtu.be/dQw4w9WgXcQ",
-        color=discord.Color.from_rgb(191, 187, 188),
+        color=discord.Color.from_rgb(51, 104, 133),
     )
     embed.set_author(
         name="Fox-y",
-        icon_url="https://cdn.discordapp.com/attachments/853168510440439821/862710006151118848/gojo.jpg",
+        icon_url="https://cdn.discordapp.com/attachments/853168510440439821/888317583848255518/fox-y.png",
     )
     embed.set_thumbnail(
-        url="https://cdn.discordapp.com/attachments/853168510440439821/862710006151118848/gojo.jpg"
+        url="https://cdn.discordapp.com/attachments/853168510440439821/888317583848255518/fox-y.png"
     )
     embed.add_field(name="**Say**", value="`|say <message>`", inline=True)
     embed.add_field(
@@ -59,33 +58,55 @@ async def help(ctx):
         value="`|talk <message>`",
         inline=True,
     )
-    embed.add_field(name="**Ramu**", value="`|ramu`", inline=True)
-
-    embed.add_field(name="**Google**", value="`|google <query>`", inline=True)
-    embed.add_field(name="**Images**", value="`|image <query>`", inline=True)
     embed.add_field(name="**Youtube**", value="`|youtube <query>`", inline=True)
-
+    embed.add_field(name="**Emoji**", value="`|emoji <emote name>`", inline=True)
+    embed.add_field(name="**Hug**", value="`|hug <person>`", inline=True)
+    embed.add_field(name="**Gif**", value="`|gif <query>`", inline=True)
     embed.add_field(
         name="**Download**",
         value="`|download <url> <audio/video>`",
         inline=False,
     )
+
     await ctx.send(embed=embed)
 
 
 @bot.event
 async def on_message(message):
     if bot.user.mentioned_in(message) and "@!862927269822464040" in message.content:
-        await message.channel.send("My prefix here is | " + message.author.mention)
+        await message.channel.send(
+            ":ramukiss: My prefix here is | " + message.author.mention
+        )
     await bot.process_commands(message)
 
 
-# @bot.command(name="calc")
-# async def calc(ctx, *args):
-#     try:
-#         await ctx.send(calc_string(" ".join(args)))
-#     except:
-#         await ctx.send("Please enter only 2 values!")
+@bot.command(name="hug")
+async def hug(ctx, arg=""):
+
+    embed = discord.Embed(colour=discord.Color.from_rgb(51, 104, 133))
+    session = aiohttp.ClientSession()
+    response = await session.get(
+        "http://api.giphy.com/v1/gifs/search?q="
+        + "+hug"
+        + f"&api_key={os.getenv('giphytoken')}&limit=10"
+    )
+    data = json.loads(await response.text())
+    gif_choice = random.randint(0, 9)
+    embed.set_image(url=data["data"][gif_choice]["images"]["original"]["url"])
+
+    await session.close()
+
+    await ctx.send(f"{ctx.message.author.mention} hugs {arg}")
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def emoji(ctx, emojiname):
+    for i in bot.guilds:
+        emoji = discord.utils.get(i.emojis, name=emojiname)
+        if emoji is not None:
+            await ctx.message.delete()
+            await ctx.send(emoji)
 
 
 @bot.command(name="say")
@@ -94,14 +115,14 @@ async def say(ctx, *args):
         channel = await bot.fetch_channel(args[0][2:-1])
         if channel:
             embed = discord.Embed(
-                title=" ".join(args[1:]), color=discord.Color.from_rgb(191, 187, 188)
+                title=" ".join(args[1:]), color=discord.Color.from_rgb(51, 104, 133)
             )
             await channel.send(embed=embed)
     else:
         channel = discord.utils.get(ctx.guild.text_channels, name=args[0])
         if channel:
             embed = discord.Embed(
-                title=" ".join(args[1:]), color=discord.Color.from_rgb(191, 187, 188)
+                title=" ".join(args[1:]), color=discord.Color.from_rgb(51, 104, 133)
             )
             await channel.send(embed=embed)
         else:
@@ -110,42 +131,47 @@ async def say(ctx, *args):
 
 @bot.command(name="talk")
 async def talk(ctx, *args):
+
     if "nigger" in args:
         Text_to_speech("i won't say that word.")
+
     else:
         Text_to_speech(" ".join(args))
+
     await ctx.send(file=discord.File("tts.mp3"))
     os.remove("tts.mp3")
 
 
 @bot.command(name="download")
+@commands.cooldown(1, 30, commands.BucketType.user)
 async def download(ctx, url, arg=None):
-    await ctx.send("Processing request...")
-    await ctx.send("Downloading be slow :sweat_smile:")
-    format_ = arg
-    case = ytdl(url, format_)
-    print(case)
-    if case[0] == 1 or case[0] == 2 or case[0] == 3:
-        await ctx.send(file=discord.File("some.mp4"))
-        await ctx.send("Process completed, mp4!")
-        os.remove("some.mp4")
-    elif case[0] == 4:
-        await ctx.send(file=discord.File("some.mp3"))
-        await ctx.send("Process completed, mp3!")
-        os.remove("some.mp3")
-    elif case[0] == 5:
-        await ctx.send("Sorry, file too big!")
-
-
-@bot.command(name="ramu")
-async def ramu(ctx):
-    image = choice(photo)
-    embed = discord.Embed(
-        color=discord.Color.from_rgb(191, 187, 188),
-    )
-    file = discord.File(image)
-    embed.set_image(url=f"attachment://{image}")
-    await ctx.send(file=file, embed=embed)
+    try:
+        await ctx.send("Processing request...")
+        format_ = arg
+        case = ytdl(url, format_)
+        print(case[1])
+        if case[0] == 1 or case[0] == 2 or case[0] == 3:
+            await ctx.send("Downloading be slow :sweat_smile:")
+            await ctx.send(file=discord.File("some.mp4"))
+            await ctx.send("Process completed, mp4!")
+            os.remove("some.mp4")
+        elif case[0] == 4:
+            await ctx.send(file=discord.File("some.mp3"))
+            await ctx.send("Process completed, mp3!")
+            os.remove("some.mp3")
+        elif case[0] == 5:
+            await ctx.send("Sorry, file too big!")
+    except Exception as _:
+        embed = discord.Embed(
+            title="You are on cooldown. Try again in 30s",
+            color=discord.Color.from_rgb(51, 104, 133),
+        )
+        embed.add_field(
+            name="_unlock premium for lowered cooldown times ;)_",
+            value="ples",
+            inline=True,
+        )
+        await ctx.send(embed=embed)
 
 
 @bot.command(name="youtube")
@@ -153,25 +179,24 @@ async def youtube_(ctx, *args):
     await ctx.send(youtube(" ".join(args)))
 
 
-@bot.command(name="google")
-async def google_search(ctx, *args):
-    embed = discord.Embed(
-        title="Search results",
-        color=discord.Color.from_rgb(191, 187, 188),
+@bot.command(name="gif", pass_context=True)
+async def giphy(ctx, *, search="random"):
+    embed = discord.Embed(colour=discord.Color.from_rgb(51, 104, 133))
+    session = aiohttp.ClientSession()
+
+    search.replace(" ", "+")
+    response = await session.get(
+        "http://api.giphy.com/v1/gifs/search?q="
+        + search
+        + f"&api_key={os.getenv('giphytoken')}&limit=10"
     )
-    for index, url in enumerate(google(" ".join(args))):
-        embed.add_field(name="\u200b", value=f"**{index+1}** .<{url}>", inline=False)
+    data = json.loads(await response.text())
+    gif_choice = random.randint(0, 9)
+    embed.set_image(url=data["data"][gif_choice]["images"]["original"]["url"])
+
+    await session.close()
+
     await ctx.send(embed=embed)
-
-
-@bot.command(name="image")
-async def google_image(ctx, *args):
-    embed = discord.Embed(color=discord.Color.from_rgb(191, 187, 188))
-    if (url := get_google_img(" ".join(args))) != None:
-        embed.set_image(url=url)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Could not find any image!")
 
 
 bot.run(os.getenv("TOKEN"))
--- downloader.py
+++ downloader.py
@@ -8,23 +8,21 @@ def ytdl(url, file_format=None):
         if (format_id := format_check(url)).isnumeric():
             os.system(f"youtube-dl -f {format_id} -o some.%(ext)s {url}")
             if "some.mp4" in os.listdir():
-                print("already mp4.")
-                return [1, "already mp4"]
+                return [1, "Already mp4"]
             elif "some.mkv" in os.listdir():
                 convert_to_mp4("some.mkv")
                 os.remove("some.mkv")
-                print("converted to mp4.")
-                return [2, "converted from mkv to mp4"]
+                return [2, "Converted from mkv to mp4"]
             else:
                 convert_to_mp4("some.webm")
                 os.remove("some.webm")
                 print("converted to mp4.")
-                return [3, "converted from webm to mp4"]
+                return [3, "Converted from webm to mp4"]
         else:
             return [5, "File too big"]
     elif file_format == "audio":
         if (format_id := audio_format(url)).isnumeric():
             os.system(f"youtube-dl  -f {format_id} -o some.mp3 {url}")
-            return [4, "downloaded mp3"]
+            return [4, "Downloaded mp3"]
         else:
             return [5, "File too big"]
